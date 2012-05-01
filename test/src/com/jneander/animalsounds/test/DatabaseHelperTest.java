fcc5442e285479d04c3c81afa21659f41d3c565e
Jeremy Neander <subjectego@gmail.com>
2012-05-01 03:37:30 -0500

    created classes DatabaseHelper and DatabaseHelperTest

---
+++ test/src/com/jneander/animalsounds/test/DatabaseHelperTest.java
@@ -0,0 +1,16 @@
+package com.jneander.animalsounds.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.jneander.animalsounds.main.MainActivity;
+
+public class DatabaseHelperTest extends ActivityInstrumentationTestCase2< MainActivity > {
+
+  public DatabaseHelperTest() {
+    super( MainActivity.class.getPackage().getName(), MainActivity.class );
+  }
+
+  protected void setUp() throws Exception {
+
+  }
+}
