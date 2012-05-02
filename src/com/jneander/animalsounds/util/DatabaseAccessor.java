1fb08b69a124ad9b4b40a008c7d321f53b0358a4
Jeremy Neander <subjectego@gmail.com>
2012-05-02 18:13:28 -0500

    added testAccessorAnimalNames() to DatabaseAccessorTest

--- src/com/jneander/animalsounds/util/DatabaseAccessor.java
+++ src/com/jneander/animalsounds/util/DatabaseAccessor.java
@@ -1,5 +1,15 @@
 package com.jneander.animalsounds.util;
 
-public class DatabaseAccessor {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
+public class DatabaseAccessor {
+  private SQLiteDatabase database;
+  private DatabaseHelper dbHelper;
+  private Context context;
+
+  public DatabaseAccessor( Context context ) {
+    this.context = context;
+    dbHelper = new DatabaseHelper( context );
+  }
 }
--- test/src/com/jneander/animalsounds/test/DatabaseAccessorTest.java
+++ test/src/com/jneander/animalsounds/test/DatabaseAccessorTest.java
@@ -3,8 +3,10 @@ package com.jneander.animalsounds.test;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.jneander.animalsounds.main.MainActivity;
+import com.jneander.animalsounds.util.DatabaseAccessor;
 
 public class DatabaseAccessorTest extends ActivityInstrumentationTestCase2< MainActivity > {
+  private MainActivity activity;
   private DatabaseAccessor dbAccessor;
   private final String[] dbAnimalsNames = new String[] { "Giraffe", "Squirrel", "Horse" };
 
@@ -14,6 +16,12 @@ public class DatabaseAccessorTest extends ActivityInstrumentationTestCase2< Main
 
   protected void setUp() throws Exception {
     super.setUp();
-
+    activity = getActivity();
+    
+    dbAccessor = new DatabaseAccessor( activity );
+  }
+  
+  public void testAccessorAnimalNames() {
+    
   }
 }
