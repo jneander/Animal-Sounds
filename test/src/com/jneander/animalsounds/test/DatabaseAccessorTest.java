ec00dca7dc3431a1524672a9d8f152f5e0876c67
Jeremy Neander <subjectego@gmail.com>
2012-05-02 18:03:17 -0500

    added DatabaseAccessor

--- test/src/com/jneander/animalsounds/test/DatabaseAccessorTest.java
+++ test/src/com/jneander/animalsounds/test/DatabaseAccessorTest.java
@@ -5,7 +5,8 @@ import android.test.ActivityInstrumentationTestCase2;
 import com.jneander.animalsounds.main.MainActivity;
 
 public class DatabaseAccessorTest extends ActivityInstrumentationTestCase2< MainActivity > {
-  
+  private DatabaseAccessor dbAccessor;
+  private final String[] dbAnimalsNames = new String[] { "Giraffe", "Squirrel", "Horse" };
 
   public DatabaseAccessorTest() {
     super( MainActivity.class.getPackage().getName(), MainActivity.class );
