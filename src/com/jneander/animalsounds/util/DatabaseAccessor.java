dfdfa835b74cbadf805ad66c4d7ec894da1ee147
Jeremy Neander <subjectego@gmail.com>
2012-05-02 23:01:36 -0500

    added getAnimalsArray() to DatabaseAccessor

--- src/com/jneander/animalsounds/util/DatabaseAccessor.java
+++ src/com/jneander/animalsounds/util/DatabaseAccessor.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.jneander.animalsounds.obj.Animal;
+
 public class DatabaseAccessor {
   private SQLiteDatabase database;
   private DatabaseHelper dbHelper;
@@ -16,20 +18,51 @@ public class DatabaseAccessor {
 
   public String[] getAnimalsNames() {
     database = dbHelper.getReadableDatabase();
-    Cursor cursor = database.rawQuery( "SELECT * FROM animals", null );
-    
+    Cursor cursor = getAnimalsCursor();
+
     int entryCount = cursor.getCount();
     String[] outputArray = new String[entryCount];
-    
+
     cursor.moveToFirst();
-    for ( int entryIndex = 0; entryIndex < entryCount; entryIndex++) {
+    for ( int entryIndex = 0; entryIndex < entryCount; entryIndex++ ) {
       outputArray[entryIndex] = cursor.getString( 1 );
       cursor.moveToNext();
     }
-    
+
     cursor.close();
     dbHelper.close();
-    
+
     return outputArray;
+  }
+
+  public Animal[] getAnimalsArray() {
+    database = dbHelper.getReadableDatabase();
+    Cursor cursor = getAnimalsCursor();
+
+    int entryCount = cursor.getCount();
+    Animal[] outputArray = new Animal[entryCount];
+
+    cursor.moveToFirst();
+    for ( int entryIndex = 0; entryIndex < entryCount; entryIndex++ ) {
+      Animal currentAnimal = new Animal();
+
+      currentAnimal.setId( cursor.getInt( 0 ) );
+      currentAnimal.setName( cursor.getString( 1 ) );
+      currentAnimal.setFacts( cursor.getString( 2 ) );
+      currentAnimal.setImagefile( cursor.getString( 3 ) );
+      currentAnimal.setSoundfile( cursor.getString( 4 ) );
+
+      outputArray[entryIndex] = currentAnimal;
+      cursor.moveToNext();
+    }
+
+    cursor.close();
+    dbHelper.close();
+
+    return outputArray;
+  }
+
+  private Cursor getAnimalsCursor() {
+    return database.rawQuery( "SELECT * FROM animals", null );
   }
 }
