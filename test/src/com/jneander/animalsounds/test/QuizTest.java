3162aeeee3252e637a5f0b2e9f1b56e058f8e531
Jeremy Neander <subjectego@gmail.com>
2012-05-06 17:30:59 -0500

    added testGetUniqueChoices() to QuizTest

--- test/src/com/jneander/animalsounds/test/QuizTest.java
+++ test/src/com/jneander/animalsounds/test/QuizTest.java
@@ -3,10 +3,12 @@ package com.jneander.animalsounds.test;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.jneander.animalsounds.main.QuizActivity;
+import com.jneander.animalsounds.obj.Animal;
+import com.jneander.animalsounds.obj.Quiz;
 
 public class QuizTest extends ActivityInstrumentationTestCase2< QuizActivity > {
-
   private QuizActivity activity;
+  private Quiz quiz;
 
   public QuizTest() {
     super( QuizActivity.class.getPackage().getName(), QuizActivity.class );
@@ -16,5 +18,25 @@ public class QuizTest extends ActivityInstrumentationTestCase2< QuizActivity > {
   protected void setUp() throws Exception {
     super.setUp();
     activity = getActivity();
+
+    quiz = new Quiz( activity );
+  }
+
+  public void testGetUniqueChoices() {
+    Animal[] quizChoices = quiz.getChoices();
+    assertNotNull( quizChoices );
+
+    assertFalse( hasDuplicates( quizChoices ) );
+  }
+
+  public < T > boolean hasDuplicates( T[] array ) {
+    boolean result = false;
+
+    for ( int firstIndex = 0; firstIndex < array.length - 1; firstIndex++ )
+      for ( int secondIndex = firstIndex + 1; secondIndex < array.length; secondIndex++ )
+        if ( array[firstIndex] == array[secondIndex] )
+          result = true;
+
+    return result;
   }
 }
