f2a62cdcd031aa77c919f4591f349cb61ded3a86
Jeremy Neander <subjectego@gmail.com>
2012-05-05 23:36:32 -0500

    added layout and related xml elements for QuizActivity

--- gen/com/jneander/animalsounds/R.java
+++ gen/com/jneander/animalsounds/R.java
@@ -14,8 +14,11 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int activity_bg=0x7f050000;
+    }
     public static final class dimen {
-        public static final int defaultMargin=0x7f060000;
+        public static final int defaultMargin=0x7f070000;
     }
     public static final class drawable {
         public static final int animal_black=0x7f020000;
@@ -28,23 +31,31 @@ public final class R {
         public static final int zoo_background_tile=0x7f020007;
     }
     public static final class id {
-        public static final int image_button_to_quiz=0x7f070003;
-        public static final int image_button_to_zoo=0x7f070002;
-        public static final int image_title=0x7f070001;
-        public static final int nav_section=0x7f070006;
-        public static final int rl1=0x7f070000;
-        public static final int zoo_animal_facts=0x7f070005;
-        public static final int zoo_animal_image=0x7f070007;
-        public static final int zoo_animal_name=0x7f070004;
-        public static final int zoo_last_button=0x7f070008;
-        public static final int zoo_next_button=0x7f070009;
+        public static final int image_button_to_quiz=0x7f080003;
+        public static final int image_button_to_zoo=0x7f080002;
+        public static final int image_title=0x7f080001;
+        public static final int nav_section=0x7f08000d;
+        public static final int quiz_animal_facts=0x7f080004;
+        public static final int quiz_animal_image=0x7f080009;
+        public static final int quiz_choice_1=0x7f080005;
+        public static final int quiz_choice_2=0x7f080006;
+        public static final int quiz_choice_3=0x7f080007;
+        public static final int quiz_choice_4=0x7f080008;
+        public static final int quiz_next_button=0x7f08000a;
+        public static final int rl1=0x7f080000;
+        public static final int zoo_animal_facts=0x7f08000c;
+        public static final int zoo_animal_image=0x7f08000e;
+        public static final int zoo_animal_name=0x7f08000b;
+        public static final int zoo_last_button=0x7f08000f;
+        public static final int zoo_next_button=0x7f080010;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int zoo=0x7f030001;
+        public static final int quiz=0x7f030001;
+        public static final int zoo=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f060001;
+        public static final int hello=0x7f060000;
     }
 }
