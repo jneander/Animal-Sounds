34feb074a0d206a816d8ca019dd51314fbaa755d
Jeremy Neander <subjectego@gmail.com>
2012-05-24 13:26:22 -0500

    added: 'testImagesArrayHasValidAssetNames()' test to 'AnimalLoaderTest' class; all tests pass; refactored

--- test/src/com/jneander/animalsounds/test/junit3/AnimalLoaderTest.java
+++ test/src/com/jneander/animalsounds/test/junit3/AnimalLoaderTest.java
@@ -1,12 +1,14 @@
 package com.jneander.animalsounds.test.junit3;
 
+import java.io.IOException;
+
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 
 import com.jneander.animalsounds.R;
 import com.jneander.animalsounds.main.MainActivity;
 
 public class AnimalLoaderTest extends ActivityInstrumentationTestCase2< MainActivity > {
+  private final String TAG = "AnimalLoaderTest";
   private MainActivity activity;
 
   public AnimalLoaderTest() {
@@ -19,22 +21,40 @@ public class AnimalLoaderTest extends ActivityInstrumentationTestCase2< MainActi
   }
 
   public void testCanAccessArrayOfAnimalNames() {
-    String[] animal_names = activity.getResources().getStringArray( R.array.animal_names );
-    assertTrue( animal_names.length > 0 );
+    String[] animalNames = activity.getResources().getStringArray( R.array.animal_names );
+    assertTrue( animalNames.length > 0 );
   }
 
   public void testCanAccessArrayOfAnimalFacts() {
-    String[] animal_facts = activity.getResources().getStringArray( R.array.animal_facts );
-    assertTrue( animal_facts.length > 0 );
+    String[] animalFacts = activity.getResources().getStringArray( R.array.animal_facts );
+    assertTrue( animalFacts.length > 0 );
   }
 
   public void testCanAccessArrayOfAnimalImages() {
-    String[] animal_images = activity.getResources().getStringArray( R.array.animal_images );
-    assertTrue( animal_images.length > 0 );
+    String[] animalImages = activity.getResources().getStringArray( R.array.animal_images );
+    assertTrue( animalImages.length > 0 );
   }
 
   public void testCanAccessArrayOfAnimalSounds() {
-    String[] animal_sounds = activity.getResources().getStringArray( R.array.animal_sounds );
-    assertTrue( animal_sounds.length > 0 );
+    String[] animalSounds = activity.getResources().getStringArray( R.array.animal_sounds );
+    assertTrue( animalSounds.length > 0 );
+  }
+
+  public void testImagesArrayHasValidAssetNames() throws IOException {
+    String[] animalImages = activity.getResources().getStringArray( R.array.animal_images );
+    String[] imageAssets = activity.getAssets().list( "images" );
+
+    checkListedAssetsExist( animalImages, imageAssets );
+  }
+
+  private void checkListedAssetsExist( String[] requiredArray, String[] assetArray ) {
+    for ( int reqIndex = 0; reqIndex < requiredArray.length; reqIndex++ ) {
+      boolean matchFound = false;
+
+      for ( int assetIndex = 0; assetIndex < assetArray.length; assetIndex++ )
+        matchFound |= assetArray[assetIndex].equals( requiredArray[reqIndex] );
+
+      assertTrue( matchFound );
+    }
   }
 }
