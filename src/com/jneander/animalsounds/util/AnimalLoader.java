1d745f3cf03d707ca73edcf16076ef5f80c4a739
Jeremy Neander <subjectego@gmail.com>
2012-05-24 13:59:12 -0500

    changed: constructor and 'loadAnimalsFromResources()' now take a 'Context' object as an argument

--- src/com/jneander/animalsounds/util/AnimalLoader.java
+++ src/com/jneander/animalsounds/util/AnimalLoader.java
@@ -1,6 +1,6 @@
 package com.jneander.animalsounds.util;
 
-import android.app.Activity;
+import android.content.Context;
 
 import com.jneander.animalsounds.R;
 import com.jneander.animalsounds.obj.Animal;
@@ -8,19 +8,19 @@ import com.jneander.animalsounds.obj.Animal;
 public class AnimalLoader {
   private Animal[] animals;
 
-  public AnimalLoader( Activity activity ) {
-    loadAnimalsFromResources( activity );
+  public AnimalLoader( Context context ) {
+    loadAnimalsFromResources( context );
   }
 
   public Animal[] getAnimalArray() {
     return this.animals;
   }
-  
-  private void loadAnimalsFromResources( Activity activity ) {
-    String[] animalNames = activity.getResources().getStringArray( R.array.animal_names );
-    String[] animalFacts = activity.getResources().getStringArray( R.array.animal_facts );
-    String[] animalImages = activity.getResources().getStringArray( R.array.animal_images );
-    String[] animalSounds = activity.getResources().getStringArray( R.array.animal_sounds );
+
+  private void loadAnimalsFromResources( Context context ) {
+    String[] animalNames = context.getResources().getStringArray( R.array.animal_names );
+    String[] animalFacts = context.getResources().getStringArray( R.array.animal_facts );
+    String[] animalImages = context.getResources().getStringArray( R.array.animal_images );
+    String[] animalSounds = context.getResources().getStringArray( R.array.animal_sounds );
 
     animals = new Animal[animalNames.length];
 
