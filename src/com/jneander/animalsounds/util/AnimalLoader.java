45385fc74a0ad92000f6e2d2770a18425ea48a5f
Jeremy Neander <subjectego@gmail.com>
2012-05-24 13:43:11 -0500

    added: constructor and 'loadAnimalsFromResources()' to 'AnimalLoader' class

--- src/com/jneander/animalsounds/util/AnimalLoader.java
+++ src/com/jneander/animalsounds/util/AnimalLoader.java
@@ -1,5 +1,34 @@
 package com.jneander.animalsounds.util;
 
+import android.app.Activity;
+
+import com.jneander.animalsounds.R;
+import com.jneander.animalsounds.obj.Animal;
+
 public class AnimalLoader {
-  
+  private Animal[] animals;
+
+  public AnimalLoader( Activity activity ) {
+    loadAnimalsFromResources( activity );
+  }
+
+  private void loadAnimalsFromResources( Activity activity ) {
+    String[] animalNames = activity.getResources().getStringArray( R.array.animal_names );
+    String[] animalFacts = activity.getResources().getStringArray( R.array.animal_facts );
+    String[] animalImages = activity.getResources().getStringArray( R.array.animal_images );
+    String[] animalSounds = activity.getResources().getStringArray( R.array.animal_sounds );
+
+    animals = new Animal[animalNames.length];
+
+    for ( int animalIndex = 0; animalIndex < animalNames.length; animalIndex++ ) {
+      Animal animal = new Animal();
+
+      animal.setName( animalNames[animalIndex] );
+      animal.setFacts( animalFacts[animalIndex] );
+      animal.setImagefile( animalImages[animalIndex] );
+      animal.setSoundfile( animalSounds[animalIndex] );
+
+      animals[animalIndex] = animal;
+    }
+  }
 }
