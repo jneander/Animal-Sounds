bafe14ed6d565be969f1a0a3800f88917629009c
Jeremy Neander <subjectego@gmail.com>
2012-05-24 11:16:36 -0500

    refactored: moved OnClick assignments of main menu buttons into MainActivity class

--- src/com/jneander/animalsounds/main/MainActivity.java
+++ src/com/jneander/animalsounds/main/MainActivity.java
@@ -4,23 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.jneander.animalsounds.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
   @Override
   public void onCreate( Bundle savedInstanceState ) {
     super.onCreate( savedInstanceState );
     setContentView( R.layout.main );
+    
+    this.findViewById( R.id.image_button_to_zoo ).setOnClickListener( this );
+    this.findViewById( R.id.image_button_to_quiz ).setOnClickListener( this );
   }
 
-  public void goToZoo( View v ) {
+  public void goToZoo() {
     Intent zooIntent = new Intent( this, ZooActivity.class );
     this.startActivity( zooIntent );
   }
-  
-  public void goToQuiz( View v ) {
+
+  public void goToQuiz() {
     Intent quizIntent = new Intent( this, QuizActivity.class );
     this.startActivity( quizIntent );
+  }
+
+  @Override
+  public void onClick( View view ) {
+    switch ( view.getId() ) {
+    case R.id.image_button_to_zoo:
+      goToZoo();
+      break;
+    case R.id.image_button_to_quiz:
+      goToQuiz();
+      break;
+    }
   }
 }
